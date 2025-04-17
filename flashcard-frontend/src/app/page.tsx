d7856739034331e77b2b49623550c0ed33cf2748
wyjee <wooyeon.jee@gmail.com>
2025-04-17 20:06:11 +0900

    [fe] temp: add test console

--- flashcard-frontend/src/app/page.tsx
+++ flashcard-frontend/src/app/page.tsx
@@ -3,11 +3,16 @@ import TopicListItem from "@/components/topic/TopicListItem";
 import PageWrapper from "@/components/layout/PageWrapper";
 import {useTopics} from "@/hooks/useTopics";
 import {useCurrentUser} from "@/hooks/useCurrentUser";
+import { useEffect } from "react";
 
 export default function Home() {
     const name = 'Home'
     const {data: currentUser} = useCurrentUser();
     const {data: topics, isLoading, isError} = useTopics();
+
+    useEffect(() => {
+        console.log("[테스트] API BASE:", process.env.NEXT_PUBLIC_API_BASE_URL);
+    }, []);
 
     if (isLoading) return <p>Loading...</p>;
     if (isError || !topics) return <p>Something went wrong</p>;
