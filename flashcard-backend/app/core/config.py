ba9d169dcde9b039be39f40bdceee903aa11b00c
wyjee <wooyeon.jee@gmail.com>
2025-04-17 19:28:10 +0900

    [fe/be] deploy: replace .env to chart values

--- flashcard-backend/app/core/config.py
+++ flashcard-backend/app/core/config.py
@@ -1,11 +1,11 @@
 import os
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
 env_name = os.getenv("ENV", "local")
 env_file = f".env.{env_name}"
-load_dotenv(dotenv_path=env_file)
+# load_dotenv(dotenv_path=env_file)
 
 class Settings(BaseSettings):
     ENV: str = os.getenv("ENV", "local")
