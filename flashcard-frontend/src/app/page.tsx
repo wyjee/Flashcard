ae1a4383d813391a4a82f6a21d95173c404e19ed
wyjee <wooyeon.jee@gmail.com>
2025-04-20 15:29:43 +0900

    [fe] test: log debgging

--- flashcard-frontend/src/app/page.tsx
+++ flashcard-frontend/src/app/page.tsx
@@ -3,7 +3,6 @@ import TopicListItem from "@/components/topic/TopicListItem";
 import PageWrapper from "@/components/layout/PageWrapper";
 import {useTopics} from "@/hooks/useTopics";
 import {useCurrentUser} from "@/hooks/useCurrentUser";
-import { useEffect } from "react";
 
 export default function Home() {
     const name = 'Home'
