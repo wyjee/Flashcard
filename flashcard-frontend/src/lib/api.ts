c3ee931eb111f7874fa39e9ec6e49520c1683f22
wyjee <wooyeon.jee@gmail.com>
2025-04-23 18:07:07 +0900

    [fe] fix: add withTailingSlash only if GET request

--- flashcard-frontend/src/lib/api.ts
+++ flashcard-frontend/src/lib/api.ts
@@ -1,6 +1,5 @@
 import axios, {AxiosError} from 'axios';
 import Cookies from 'js-cookie';
-import {withTrailingSlash} from '@/lib/utils';
 
 console.log("[테스트] API.ts:", process.env.NEXT_PUBLIC_API_BASE_URL);
 
@@ -14,8 +13,14 @@ const api = axios.create({
 
 // 요청 인터셉터: URL 끝에 슬래시 붙이기
 api.interceptors.request.use((config) => {
-    if (config.url && !config.url.startsWith('/auth')) {
-        config.url = withTrailingSlash(config.url);
+    console.log(config.method)
+    if (
+        config.method === 'get' &&
+        config.url &&
+        !config.url.startsWith('/auth') &&
+        !config.url.endsWith('/')
+    ) {
+        config.url += '/';
     }
     return config;
 });
