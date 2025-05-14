f249a43c412990b267cddf4c0fc61d2ad76c7cc9
wyjee <wooyeon.jee@gmail.com>
2025-05-15 00:00:08 +0900

    [fe] test: modify home page title

--- flashcard-frontend/src/app/page.tsx
+++ flashcard-frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ import {useTopics} from "@/hooks/useTopics";
 import {useCurrentUser} from "@/hooks/useCurrentUser";
 
 export default function Home() {
-    const name = 'Topics (updated v18...)'
+    const name = 'Topics (updated v19...)'
     const {data: currentUser} = useCurrentUser();
     const {data: topics, isLoading, isError} = useTopics();
 
